6d556c38eda72336cee41a47445f543def6cd712
Lidan <ls301205@gmail.com>
2023-09-02 20:55:52 +0300

    added mana regen

--- src/main/java/me/lidan/cavecrawlers/stats/StatsManager.java
+++ src/main/java/me/lidan/cavecrawlers/stats/StatsManager.java
@@ -39,10 +39,23 @@ public class StatsManager {
         player.setHealthScale(40);
         double maxHealth = stats.get(StatType.HEALTH).getValue();
         player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(maxHealth);
-        double regen = ((maxHealth * 0.01) + 1.5);
+
+        // speed
+        double speed = stats.get(StatType.SPEED).getValue();
+        player.setWalkSpeed((float) (speed/500));
+
+        // health regen
+        double healthRegen = ((maxHealth * 0.01) + 1.5);
         double health = player.getHealth();
-        player.setHealth(Math.min(health + regen, maxHealth));
+        player.setHealth(Math.min(health + healthRegen, maxHealth));
         player.setFoodLevel(200);
+
+        // mana regen
+        double intel = stats.get(StatType.INTELLIGENCE).getValue();
+        Stat manaStat = stats.get(StatType.MANA);
+        double mana = manaStat.getValue();
+        double manaRegen = intel * 0.02;
+        manaStat.setValue(Math.min(mana + manaRegen, intel));
     }
 
     public void statLoop(){

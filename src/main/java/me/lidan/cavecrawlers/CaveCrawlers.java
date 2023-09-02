5d46c1475662d4b7716bc5b24dd4c11d696d9f96
Lidan <ls301205@gmail.com>
2023-09-02 14:48:18 +0300

    added stats list to list player stats added CommandHandler to handle commands

--- src/main/java/me/lidan/cavecrawlers/CaveCrawlers.java
+++ src/main/java/me/lidan/cavecrawlers/CaveCrawlers.java
@@ -1,17 +1,31 @@
 package me.lidan.cavecrawlers;
 
+import me.lidan.cavecrawlers.commands.StatCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+import revxrsal.commands.CommandHandler;
+import revxrsal.commands.bukkit.BukkitCommandHandler;
 
 public final class CaveCrawlers extends JavaPlugin {
+
+    private CommandHandler commandHandler;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        commandHandler = BukkitCommandHandler.create(this);
 
+    }
+
+    public void registerCommands(){
+        commandHandler.register(new StatCommand());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static CaveCrawlers getInstance(){
+        return CaveCrawlers.getPlugin(CaveCrawlers.class);
     }
 }
--- src/main/java/me/lidan/cavecrawlers/commands/StatCommand.java
+++ src/main/java/me/lidan/cavecrawlers/commands/StatCommand.java
@@ -1,10 +1,23 @@
 package me.lidan.cavecrawlers.commands;
 
+import me.lidan.cavecrawlers.stats.Stats;
+import me.lidan.cavecrawlers.stats.StatsManager;
+import org.bukkit.entity.Player;
 import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Default;
+import revxrsal.commands.annotation.Subcommand;
 import revxrsal.commands.bukkit.annotation.CommandPermission;
 
 @Command({"stats", "stat", "statadmin"})
 @CommandPermission("stats.admin")
 public class StatCommand {
 
+    @Subcommand("list")
+    public void listStats(Player sender, Player arg){
+        if(arg == null) {
+            arg = sender;
+        }
+        Stats stats = StatsManager.getInstance().getStats(arg);
+        sender.sendMessage(stats.toFormatString());
+    }
 }

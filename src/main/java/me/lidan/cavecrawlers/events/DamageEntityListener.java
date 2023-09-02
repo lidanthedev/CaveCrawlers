19742a844cb682315837b4989c691476477b4aba
Lidan <ls301205@gmail.com>
2023-09-02 20:32:03 +0300

    implemented onPlayerDamaged

--- src/main/java/me/lidan/cavecrawlers/events/DamageEntityListener.java
+++ src/main/java/me/lidan/cavecrawlers/events/DamageEntityListener.java
@@ -1,10 +1,13 @@
 package me.lidan.cavecrawlers.events;
 
 import me.lidan.cavecrawlers.damage.PlayerDamageCalculation;
+import me.lidan.cavecrawlers.stats.StatType;
+import me.lidan.cavecrawlers.stats.Stats;
+import me.lidan.cavecrawlers.stats.StatsManager;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Particle;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -13,18 +16,22 @@ public class DamageEntityListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player player){
-            if (event.getEntity() instanceof Mob mob){
+        if (event.getDamager() instanceof Player player) {
+            if (event.getEntity() instanceof Mob mob) {
                 onPlayerDamageMob(event, player, mob);
             }
-        } else if (event.getDamager() instanceof Mob mob) {
-            if (event.getEntity() instanceof Player player){
-                onMobDamagePlayer(event, player, mob);
+        } else if (event.getEntity() instanceof Player player) {
+            onPlayerDamaged(event, player);
+        } else if (event.getDamager() instanceof Projectile projectile) {
+            if (projectile.getShooter() instanceof Player player) {
+                if (event.getEntity() instanceof Mob mob) {
+                    onPlayerDamageMob(event, player, mob);
+                }
             }
         }
     }
 
-    private void onPlayerDamageMob(EntityDamageByEntityEvent event, Player player, Mob mob){
+    private void onPlayerDamageMob(EntityDamageByEntityEvent event, Player player, Mob mob) {
         PlayerDamageCalculation calculation = new PlayerDamageCalculation(player);
         double damage = calculation.calculate();
         boolean crit = calculation.isCrit();
@@ -32,10 +39,9 @@ public class DamageEntityListener implements Listener {
         double finalDamage = event.getFinalDamage();
 
         StringBuilder msg = new StringBuilder();
-        if (crit){
+        if (crit) {
             msg.append(ChatColor.WHITE).append("✧").append(finalDamage).append(ChatColor.WHITE).append("✧");
-        }
-        else{
+        } else {
             msg.append(ChatColor.WHITE).append(finalDamage);
         }
 
@@ -44,7 +50,15 @@ public class DamageEntityListener implements Listener {
 
     }
 
-    private void onMobDamagePlayer(EntityDamageByEntityEvent event, Player player, Mob mob) {
+    private void onPlayerDamaged(EntityDamageByEntityEvent event, Player player) {
+        double damage = event.getDamage();
+        Stats stats = StatsManager.getInstance().getStats(player);
+        double defense = stats.get(StatType.DEFENSE).getValue();
+        double damageReduction = defense / (defense + 100);
 
+        damage = damage * (1 - damageReduction);
+        player.sendMessage("DAMAGED: " + damage + " With RES: " + damageReduction);
+
+        event.setDamage(damage);
     }
 }

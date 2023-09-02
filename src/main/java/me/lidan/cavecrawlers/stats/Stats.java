84681bacc1c7ef855729a90f2386e1110fcd71c6
Lidan <ls301205@gmail.com>
2023-09-02 14:21:37 +0300

    implemented Stats

---
+++ src/main/java/me/lidan/cavecrawlers/stats/Stats.java
@@ -0,0 +1,96 @@
+package me.lidan.cavecrawlers.stats;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class Stats implements Iterable<Stat> {
+    private final Map<StatType, Stat> stats;
+
+    public Stats(List<Stat> statList) {
+        this.stats = new HashMap<>();
+        for (Stat stat : statList) {
+            this.stats.put(stat.getType(), stat);
+        }
+        for (StatType type : StatType.values()) {
+            if (!stats.containsKey(type)){
+                stats.put(type, new Stat(type));
+            }
+        }
+    }
+
+    public Stats() {
+        this(new ArrayList<>());
+    }
+
+    public Stat get(StatType type){
+        if (stats.containsKey(type)){
+            return stats.get(type);
+        }
+        throw new IllegalArgumentException("Stat type " + type + " Does not exist!");
+    }
+
+    public void set(StatType type, double amount){
+        get(type).add(amount);
+    }
+
+    public void add(StatType type, double amount){
+        get(type).add(amount);
+    }
+
+    public void remove(StatType type, double amount){
+        get(type).remove(amount);
+    }
+
+    public void multiply(StatType type, double amount){
+        get(type).multiply(amount);
+    }
+
+    public void multiply(double multiplier) {
+        for (Stat stat : this) {
+            stat.multiply(multiplier);
+        }
+    }
+
+    public void add(Stats stats) {
+        for (Stat stat : stats) {
+            this.add(stat.getType(), stat.getValue());
+        }
+    }
+
+    public List<Stat> toList() {
+        List<Stat> stats = new ArrayList<>();
+        for (StatType stat : StatTypes.STATS) {
+            stats.add(this.stats.getOrDefault(stat, new Stat(stat, 0)));
+        }
+        return stats;
+    }
+
+    @Override
+    public String toString() {
+        return this.toList().toString();
+    }
+
+    @Override
+    public Iterator<Stat> iterator() {
+        return this.toList().iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Stat> action) {
+        Objects.requireNonNull(action);
+        for (Stat e : this.toList()) {
+            action.accept(e);
+        }
+    }
+
+    @Override
+    public Spliterator<Stat> spliterator() {
+        return Spliterators.spliterator(this.toList(), Spliterator.ORDERED);
+    }
+
+    public Stream<Stat> stream() {
+        return StreamSupport.stream(spliterator(), false);
+    }
+}

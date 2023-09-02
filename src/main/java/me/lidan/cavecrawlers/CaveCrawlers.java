e92793c13e2cad0bb052d332b38de02abd1b7519
Lidan <ls301205@gmail.com>
2023-09-02 15:30:58 +0300

    removed GUI lib for now switch from paper api to spigot api Changed Color to ChatColor

--- src/main/java/me/lidan/cavecrawlers/CaveCrawlers.java
+++ src/main/java/me/lidan/cavecrawlers/CaveCrawlers.java
@@ -1,9 +1,14 @@
 package me.lidan.cavecrawlers;
 
 import me.lidan.cavecrawlers.commands.StatCommand;
+import me.lidan.cavecrawlers.stats.StatType;
 import org.bukkit.plugin.java.JavaPlugin;
 import revxrsal.commands.CommandHandler;
 import revxrsal.commands.bukkit.BukkitCommandHandler;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class CaveCrawlers extends JavaPlugin {
 
@@ -14,10 +19,15 @@ public final class CaveCrawlers extends JavaPlugin {
         // Plugin startup logic
         commandHandler = BukkitCommandHandler.create(this);
         registerCommands();
-        getSLF4JLogger().info("Loaded CaveCrawlers!");
+        getLogger().info("Loaded CaveCrawlers!");
     }
 
     public void registerCommands(){
+        commandHandler.getAutoCompleter().registerParameterSuggestions(StatType.class, (args, sender, command) -> {
+            return Arrays.stream(StatType.values())
+                    .map(StatType::name)
+                    .collect(Collectors.toList());
+        });
         commandHandler.register(new StatCommand());
     }
 
--- src/main/java/me/lidan/cavecrawlers/stats/StatType.java
+++ src/main/java/me/lidan/cavecrawlers/stats/StatType.java
@@ -1,75 +1,62 @@
 package me.lidan.cavecrawlers.stats;
 
-import org.bukkit.Color;
+import lombok.Getter;
+import net.md_5.bungee.api.ChatColor;
 
+@Getter
 public enum StatType {
-    DAMAGE("Damage", "❁", Color.RED)
+    DAMAGE("Damage", "❁", ChatColor.RED)
     ,STRENGTH("Strength",
-            "❁",Color.RED)
+            "❁",ChatColor.RED)
     ,CRIT_DAMAGE("Crit Damage",
             "☠",
-            Color.BLUE)
+            ChatColor.BLUE)
     ,CRIT_CHANCE("Crit Chance",
             "☣",
-            Color.BLUE)
+            ChatColor.BLUE)
     ,ABILITY_DAMAGE("Ability Damage",
             "๑",
-            Color.RED)
+            ChatColor.RED)
     ,HEALTH("Health",
             "❤",
-            Color.RED)
+            ChatColor.RED)
     ,DEFENSE("Defense",
             "❈",
-            Color.GREEN)
+            ChatColor.GREEN)
     ,SPEED("Speed",
             "✦",
-            Color.WHITE, 100)
+            ChatColor.WHITE, 100)
     ,INTELLIGENCE("Intelligence",
             "✎",
-            Color.AQUA, 100)
+            ChatColor.AQUA, 100)
     ,MANA("Mana",
             "✎",
-            Color.AQUA, 100)
+            ChatColor.AQUA, 100)
     ,MINING_SPEED("Mining Speed",
             "⸕",
-            Color.ORANGE)
+            ChatColor.GOLD)
     ,MINING_FORTUNE("Mining Fortune",
             "☘",
-            Color.ORANGE);
+            ChatColor.GOLD);
     private final String name;
     private final String icon;
-    private final Color color;
+    private final ChatColor color;
     private final double base;
 
-    StatType(String name, String icon, Color color) {
+
+    StatType(String name, String icon, ChatColor color) {
         this(name, icon, color, 0.0);
     }
 
-    StatType(String name, String icon, Color color, double base) {
+    StatType(String name, String icon, ChatColor color, double base) {
         this.name = name;
         this.icon = icon;
         this.color = color;
         this.base = base;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public double getBase() {
-        return base;
-    }
-
     public String getFormatName(){
-        return color + name + icon;
+        return color + icon + name;
     }
 
     public String getColoredName(){

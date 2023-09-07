8daf29a750b1c8d16bd18ef0fe76c24f04e94c8b
Lidan <ls301205@gmail.com>
2023-09-07 22:32:16 +0300

    fixed colors in StatType added more methods to ItemAbility

--- src/main/java/me/lidan/cavecrawlers/items/abilities/ItemAbility.java
+++ src/main/java/me/lidan/cavecrawlers/items/abilities/ItemAbility.java
@@ -1,20 +1,53 @@
 package me.lidan.cavecrawlers.items.abilities;
 
 import lombok.Getter;
+import me.lidan.cavecrawlers.stats.StatType;
+import me.lidan.cavecrawlers.stats.Stats;
+import me.lidan.cavecrawlers.stats.StatsManager;
+import me.lidan.cavecrawlers.utils.Cooldown;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
 
 @Getter
 public abstract class ItemAbility {
     private final String name;
     private final String description;
     private final double cost;
-    private final double cooldown;
+    private final long cooldown;
+    private final Cooldown<UUID> abilityCooldown;
 
-    public ItemAbility(String name, String description, double cost, double cooldown) {
+    public ItemAbility(String name, String description, double cost, long cooldown) {
         this.name = name;
         this.description = description;
         this.cost = cost;
         this.cooldown = cooldown;
+        this.abilityCooldown = new Cooldown<>();
     }
 
-    public abstract void useAbility();
+    public void activateAbility(Player player){
+        if (abilityCooldown.getCurrentCooldown(player.getUniqueId()) < cooldown){
+            abilityFailedCooldown(player);
+            return;
+        }
+        Stats stats = StatsManager.getInstance().getStats(player);
+        double mana = stats.get(StatType.MANA).getValue();
+        if (mana < cost){
+            abilityFailedNoMana(player);
+            return;
+        }
+
+        abilityCooldown.startCooldown(player.getUniqueId());
+        useAbility(player);
+    }
+
+    public void abilityFailedNoMana(Player player){
+
+    }
+
+    public void abilityFailedCooldown(Player player){
+
+    }
+
+    protected abstract void useAbility(Player player);
 }

5c214a0613456c8a19cd471e4f3bd406b1264bd2
ofir <ofirregev06@gmail.com>
2024-06-12 03:55:53 +0300

    fix pull request comment make some text config

--- src/main/java/me/lidan/cavecrawlers/gui/MenuGui.java
+++ src/main/java/me/lidan/cavecrawlers/gui/MenuGui.java
@@ -9,6 +9,8 @@ import org.bukkit.entity.Player;
 public class MenuGui {
     private final Player player;
     private final Gui gui;
+    public final String AFK_SKULL_TEXTURE = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWM0YTQxNTU0MzQyYjJjYmZiZDg5NWU5ZWM3MDg5YWU4NjFmZWM4ZjUxNjkzODIyZWMxZWIzY2EzZjE4In19fQ==";
+    public final String SPAWN_SKULL_TEXTURE = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZjE1MWNmZmRhZjMwMzY3MzUzMWE3NjUxYjM2NjM3Y2FkOTEyYmE0ODU2NDMxNThlNTQ4ZDU5YjJlYWQ1MDExIn19fQ==";
 
     public MenuGui(Player player) {
         this.player = player;
@@ -18,13 +20,11 @@ public class MenuGui {
 
         gui.disableAllInteractions();
         gui.setItem(13, ItemBuilder.skull().owner(player).setName("§f%s Stats:".formatted(player.getName())).setLore(statsMessage).asGuiItem());
-        gui.setItem(11, ItemBuilder.skull().texture("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWM0YTQxNTU0MzQyYjJjYmZiZDg5NWU5ZWM3MDg5YWU4NjFmZWM4ZjUxNjkzODIyZWMxZWIzY2EzZjE4In19fQ==")
-                .setName("§fWarp AFK").asGuiItem((event -> {
+        gui.setItem(11, ItemBuilder.skull().texture(AFK_SKULL_TEXTURE).setName("§fWarp AFK").asGuiItem((event -> {
             Player sender = (Player) event.getWhoClicked();
             sender.performCommand("warp afk");
         })));
-        gui.setItem(15, ItemBuilder.skull().texture("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZjE1MWNmZmRhZjMwMzY3MzUzMWE3NjUxYjM2NjM3Y2FkOTEyYmE0ODU2NDMxNThlNTQ4ZDU5YjJlYWQ1MDExIn19fQ==")
-                .setName("§fWarp Spawn").asGuiItem((event -> {
+        gui.setItem(15, ItemBuilder.skull().texture(SPAWN_SKULL_TEXTURE).setName("§fWarp Spawn").asGuiItem((event -> {
             Player sender = (Player) event.getWhoClicked();
             sender.performCommand("spawn");
         })));

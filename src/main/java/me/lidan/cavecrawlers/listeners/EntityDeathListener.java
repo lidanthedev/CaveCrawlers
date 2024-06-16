8c5f36d84e6b225739cd1de3399b2c080dc3206b
Lidan <ls301205@gmail.com>
2024-06-16 21:20:12 +0300

    fixed drops getting drops from mobs falling to void

--- src/main/java/me/lidan/cavecrawlers/listeners/EntityDeathListener.java
+++ src/main/java/me/lidan/cavecrawlers/listeners/EntityDeathListener.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 public class EntityDeathListener implements Listener {
@@ -15,7 +16,8 @@ public class EntityDeathListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onEntityDeath(EntityDeathEvent event) {
         LivingEntity entity = event.getEntity();
-        if (entity.getKiller() != null){
+        EntityDamageEvent lastDamageCause = entity.getLastDamageCause();
+        if (entity.getKiller() != null && lastDamageCause != null && lastDamageCause.getCause() != EntityDamageEvent.DamageCause.VOID){
             entityManager.onDeath(event);
             event.setDroppedExp(0);
             event.getDrops().clear();

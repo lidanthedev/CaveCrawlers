97ca537333e2dbd7b421dfc2d17dbbf6e888d3fd
Yoav <yoaverror@gmail.com>
2024-07-05 00:42:41 +0300

    Menu Name Updated

--- src/main/java/me/lidan/cavecrawlers/gui/MenuGui.java
+++ src/main/java/me/lidan/cavecrawlers/gui/MenuGui.java
@@ -11,10 +11,11 @@ import org.bukkit.entity.Player;
 public class MenuGui {
     private final Player player;
     private final Gui gui;
-    public final String AFK_SKULL_TEXTURE = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWM0YTQxNTU0MzQyYjJjYmZiZDg5NWU5ZWM3MDg5YWU4NjFmZWM4ZjUxNjkzODIyZWMxZWIzY2EzZjE4In19fQ==";
+    public final String AFK_SKULL_TEXTURE = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvODQ3YzdlYzdhNjE3MWNiOWJjZmE0Mjk1YjI2ODk4YWI1YTQ2NDQxYzc5NDFkNWFhMGJjZjMzZjQ1ZjgwMmMwMSJ9fX0=";
     public final String SPAWN_SKULL_TEXTURE = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZjE1MWNmZmRhZjMwMzY3MzUzMWE3NjUxYjM2NjM3Y2FkOTEyYmE0ODU2NDMxNThlNTQ4ZDU5YjJlYWQ1MDExIn19fQ==";
-    public final String AFK_COMMAND = "warp afk";
+    public final String ZEALOT_COUNT_COMMAND = "zealot count";
     public final String SPAWN_COMMAND = "spawn";
+
 
     public MenuGui(Player player) {
         this.player = player;
@@ -23,15 +24,15 @@ public class MenuGui {
         this.gui = new Gui(3, "§7Menu");
 
         gui.disableAllInteractions();
-        gui.setItem(13, ItemBuilder.skull().owner(player).setName("§f%s Stats:".formatted(player.getName())).setLore(statsMessage).asGuiItem());
-        gui.setItem(11, ItemBuilder.skull().texture(AFK_SKULL_TEXTURE).setName("§fWarp AFK").asGuiItem((event -> {
+        gui.setItem(14, ItemBuilder.skull().owner(player).setName("§f%s Stats:".formatted(player.getName())).setLore(statsMessage).asGuiItem());
+        gui.setItem(12, ItemBuilder.skull().texture(AFK_SKULL_TEXTURE).setName("§5Zealot Kill Counter").asGuiItem((event -> {
             Player sender = (Player) event.getWhoClicked();
-            sender.performCommand(AFK_COMMAND);
+            sender.performCommand(ZEALOT_COUNT_COMMAND);
         })));
-        gui.setItem(15, ItemBuilder.skull().texture(SPAWN_SKULL_TEXTURE).setName("§fWarp Spawn").asGuiItem((event -> {
-            Player sender = (Player) event.getWhoClicked();
-            sender.performCommand(SPAWN_COMMAND);
-        })));
+//        gui.setItem(15, ItemBuilder.skull().texture(SPAWN_SKULL_TEXTURE).setName("§fCombat Skill").asGuiItem((event -> {
+//            Player sender = (Player) event.getWhoClicked();
+//            sender.performCommand(SPAWN_COMMAND);
+//        })));
         gui.getFiller().fill(ItemBuilder.from(Material.BLACK_STAINED_GLASS_PANE).name(Component.text("")).asGuiItem());
     }
 

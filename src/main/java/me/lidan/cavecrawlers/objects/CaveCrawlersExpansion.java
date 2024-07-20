e01d3bb852ebf83ad10900567e0a1933727270b1
Lidan <ls301205@gmail.com>
2024-07-20 20:54:10 +0300

    made level placeholder work

--- src/main/java/me/lidan/cavecrawlers/objects/CaveCrawlersExpansion.java
+++ src/main/java/me/lidan/cavecrawlers/objects/CaveCrawlersExpansion.java
@@ -2,14 +2,20 @@ package me.lidan.cavecrawlers.objects;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.lidan.cavecrawlers.CaveCrawlers;
+import me.lidan.cavecrawlers.levels.LevelConfigLoader;
+import me.lidan.cavecrawlers.levels.LevelInfo;
 import me.lidan.cavecrawlers.stats.StatType;
 import me.lidan.cavecrawlers.stats.StatsManager;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CaveCrawlersExpansion extends PlaceholderExpansion {
 
+    private static final Logger log = LoggerFactory.getLogger(CaveCrawlersExpansion.class);
     private final StatsManager statsManager;
     private CaveCrawlers plugin;
 
@@ -43,6 +49,21 @@ public class CaveCrawlersExpansion extends PlaceholderExpansion {
                 StatType statType = StatType.valueOf(args[1]);
                 return String.valueOf(statsManager.getStats(player.getUniqueId()).get(statType).getValue());
             }
+        }
+        else if (args[0].equalsIgnoreCase("level")){
+            LevelConfigLoader levelconfigLoader = LevelConfigLoader.getInstance(plugin);
+            LevelInfo levelInfo = levelconfigLoader.getPlayerLevelInfo(player.getUniqueId().toString());
+            try {
+                if (levelInfo != null) {
+                    int level = levelInfo.getLevel(); // Retrieve the player's level
+                    ChatColor levelColor = ChatColor.valueOf(levelconfigLoader.getLevelColor(level)); // Get ChatColor based on level
+                    return levelColor + "" + level;
+                }
+            }
+            catch (Exception e){
+                log.warn("Error getting level for {}", player.getName());
+            }
+            return "0";
         }
         return null;
     }

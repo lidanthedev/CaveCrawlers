5a12cebdeb8bb49cada107cf90f92114986b284f
fishministrator <yulucohen@gmail.com>
2024-12-25 22:58:19 +0200

    Working on error arrowability(WIP)

--- src/main/java/me/lidan/cavecrawlers/items/abilities/ArrowAbility.java
+++ src/main/java/me/lidan/cavecrawlers/items/abilities/ArrowAbility.java
@@ -16,15 +16,25 @@ public class ArrowAbility extends ClickAbility implements Listener {
 
     @Override
     protected boolean useAbility(PlayerEvent playerEvent) {
+
         Player player = playerEvent.getPlayer();
         Location loc =  player.getEyeLocation();
         World world = player.getWorld();
         Location newloc = loc.clone();
-        for(int i = 0 ;i < 1000 ; i++) {
+        for(float i = 0 ;i < 1000 ; i++) {
             Vector vector = newloc.getDirection();
-            newloc.add(vector.multiply(0.01).normalize());
-            world.spawnParticle(Particle.SOUL_FIRE_FLAME, newloc, 1, 0, 0, 0, 0);
-            vector.setY(vector.getY()+90);
+            newloc.add(vector.multiply(0.1).normalize());
+            world.spawnParticle(Particle.FLAME, newloc, 1, 0, 0, 0, 0.01);
+
+            Location newloc2 = newloc.clone();
+            newloc2.setPitch(-90);
+            newloc2.setYaw(0);
+
+            Vector vector2 = newloc2.getDirection();
+            Location loc2 = newloc.clone().add(vector2.multiply(i/1000).normalize());
+            player.sendMessage( "" + loc2.getY());
+            world.spawnParticle(Particle.SOUL_FIRE_FLAME, loc2, 1, 0, 0, 0, 0.01);
+
         }
         return true;
     }

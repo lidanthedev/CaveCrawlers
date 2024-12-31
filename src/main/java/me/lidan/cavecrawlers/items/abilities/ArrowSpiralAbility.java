401de49f57d2ee99ec75779e32d2e69ed5a9aa2d
RoniOuzan <roniouzan@gmail.com>
2024-12-31 17:54:50 +0200

    Fixed Arrow Spiral ability

--- src/main/java/me/lidan/cavecrawlers/items/abilities/ArrowSpiralAbility.java
+++ src/main/java/me/lidan/cavecrawlers/items/abilities/ArrowSpiralAbility.java
@@ -14,92 +14,107 @@ import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import java.util.Random;
-
 public class ArrowSpiralAbility extends ClickAbility implements Listener {
 
     public ArrowSpiralAbility() {
         super("Arrow Spiral", "Shoots an arrow with a Spiral around it", 0, 0);
     }
 
-    private double projectileVelocity = 2;
-    private double maxDistance = 100;
-    private double resolution = 10;
-    private double amountOfLines = 8;
-    private double loopiness = 1;
-    private double radius = 1;
-
     private Particle mainParticle = Particle.SOUL;
     private Particle[] sideParticles = {Particle.SOUL_FIRE_FLAME, Particle.FLAME};
 
+    private double maxDistance = 50;
+    private double radius = 2;
+    private double rps = 6;
+    private double velocity = 1.5;
+    private double amount = 8;
+    private double period = 0.02;
+
+    private final long DELAY = 1L;
+    private final Vector GRAVITY = new Vector(0, -20, 0); // gravity for arrow 20 m/s^2
+
     @Override
     protected boolean useAbility(PlayerEvent playerEvent) {
-
         Player player = playerEvent.getPlayer();
-        Location loc = player.getEyeLocation();
+        Location location = player.getEyeLocation();
         World world = player.getWorld();
 
         Projectile p = player.launchProjectile(Arrow.class);
-        p.setVelocity(loc.getDirection().multiply(projectileVelocity));
+        p.setVelocity(location.getDirection().multiply(velocity));
+
 
         new BukkitRunnable() {
-            private final double mainParticleSpread = radius / 50;
-            private final double lineSpacing = 360 / amountOfLines;
-            private final double resolutionRatio = 1 / resolution;
-
-            int i = new Random().nextInt(360);
-
+            int iteration = 0;
+            double lastTime = 0;
             @Override
             public void run() {
-                Location newloc = p.getLocation();
-                for (int f = 0; f < resolution; f++) {
-                    Vector vector = p.getVelocity();
-                    newloc.add(vector.multiply(resolutionRatio));
-                    if (newloc.distance(player.getLocation()) > 2.5) {
-                        world.spawnParticle(mainParticle, newloc, 2, 0, 0, 0, mainParticleSpread);
-                        for (int a = 0, particleIndex = 0; a < amountOfLines; a++, particleIndex++) {
-                            if (particleIndex >= sideParticles.length) particleIndex = 0;
-                            Location loc2 = newloc.clone();
-
-                            float angle = (float) Math.toRadians(360 - ((i * loopiness + a * lineSpacing) % 360));
-                            loc2.add(new Vector(Math.cos(angle), Math.sin(angle), Math.cos(angle)).multiply(radius)
-                                    .rotateAroundY(Math.toRadians(loc.getYaw()))
-                                    .rotateAroundX(Math.toRadians(-p.getLocation().getPitch()))
-                            );
-                            world.spawnParticle(sideParticles[particleIndex], loc2, 1, 0, 0, 0, 0);
-                        }
-                    }
-                    i++;
-                }
-                if (p.getLocation().distance(loc) > maxDistance || p.isDead()) {
-                    p.remove();
+                double time = (iteration * DELAY) / 20.0;
+                if (time * velocity >= maxDistance || p.isDead()) {
                     cancel();
+                    return;
                 }
+
+                Location currentLocation = p.getLocation();
+                Vector vector = p.getVelocity().normalize();
+
+                Vector arbitrary = new Vector(1, 0, 0);
+                if (vector.clone().crossProduct(arbitrary).length() == 0) {
+                    arbitrary = new Vector(0, 1, 0);
+                }
+
+                Vector perp1 = vector.clone().crossProduct(arbitrary).normalize();
+                Vector perp2 = vector.clone().crossProduct(perp1).normalize();
+
+                for (double i = 0; i < DELAY / 20.0; i += period) {
+                    double currentTime = time + i;
+                    Location futureLocation = currentLocation.clone()
+                            .add(p.getVelocity().multiply(20 * i))
+                            .add(GRAVITY.clone().multiply(0.5 * i * i));
+
+                    for (int j = 0; j < amount; j++) {
+                        double angle = (j / amount) * (2 * Math.PI);
+                        angle += rps * currentTime;
+
+                        Vector circleVector = perp1.clone().multiply(Math.cos(angle))
+                                .add(perp2.clone().multiply(Math.sin(angle)))
+                                .multiply(radius);
+
+                        world.spawnParticle(sideParticles[j % 2], futureLocation.clone().add(circleVector), 1, 0, 0, 0, 0);
+                    }
+                    world.spawnParticle(mainParticle, currentLocation, 1, 0, 0, 0, 0);
+                }
+
+                lastTime = time;
+                iteration++;
             }
-        }.runTaskTimer(CaveCrawlers.getInstance(), 0, 1);
+        }.runTaskTimer(CaveCrawlers.getInstance(), 0L, DELAY);
+
         return true;
     }
 
     @Override
     public ItemAbility buildAbilityWithSettings(JsonObject map) {
         ArrowSpiralAbility ability = (ArrowSpiralAbility) super.buildAbilityWithSettings(map);
-        if (map.has("amountOfLines")) {
-            ability.amountOfLines = map.get("amountOfLines").getAsDouble();
-        }
         if (map.has("maxDistance")) {
             ability.maxDistance = map.get("maxDistance").getAsDouble();
         }
         if (map.has("radius")) {
             ability.radius = map.get("radius").getAsDouble();
         }
-        if (map.has("loopiness")) {
-            ability.loopiness = map.get("loopiness").getAsDouble();
+        if (map.has("rps")) {
+            ability.rps = map.get("rps").getAsDouble();
         }
-        if (map.has("projectileVelocity")) {
-            ability.projectileVelocity = map.get("projectileVelocity").getAsDouble();
+        if (map.has("velocity")) {
+            ability.velocity = map.get("velocity").getAsDouble();
         }
-        if (map.has("resolution")) {
-            ability.resolution = map.get("resolution").getAsDouble();
+        if (map.has("amount")) {
+            ability.amount = map.get("amount").getAsDouble();
+        }
+        if (map.has("maxDistance")) {
+            ability.maxDistance = map.get("maxDistance").getAsDouble();
+        }
+        if (map.has("period")) {
+            ability.period = map.get("period").getAsDouble();
         }
         if (map.has("mainParticle")) {
             ability.mainParticle = Particle.valueOf(map.get("mainParticle").getAsString());

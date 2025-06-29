08c5ace2f6ead1f2205fa36c369340392e1431c8
lidanthedev <ls301205@gmail.com>
2025-06-30 01:51:27 +0300

    added todo end of session

--- src/main/java/me/lidan/cavecrawlers/drops/DropLoader.java
+++ src/main/java/me/lidan/cavecrawlers/drops/DropLoader.java
@@ -2,6 +2,7 @@ package me.lidan.cavecrawlers.drops;
 
 import me.lidan.cavecrawlers.objects.ConfigLoader;
 
+// Todo: make this not a singleton and allow multiple loaders for adddon support
 public class DropLoader extends ConfigLoader<EntityDrops> {
 
     private static DropLoader instance;

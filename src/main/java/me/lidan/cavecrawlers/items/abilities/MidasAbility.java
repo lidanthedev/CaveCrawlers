ea24b481674cac3d0485c671f30f4ff46892c92a
lidanthedev <ls301205@gmail.com>
2025-12-06 02:43:13 +0200

    Fixed midas ability to work on 1.19

--- src/main/java/me/lidan/cavecrawlers/items/abilities/MidasAbility.java
+++ src/main/java/me/lidan/cavecrawlers/items/abilities/MidasAbility.java
@@ -82,12 +82,10 @@ public class MidasAbility extends ScalingClickAbility implements Listener {
     public void summonFallingBlock(Location loc) {
         World world = loc.getWorld();
         world.spawnParticle(XParticle.EXPLOSION.get(), loc, 1, 0, 0, 0, 0);
-        world.spawn(loc, FallingBlock.class, fallingBlock -> {
-            fallingBlock.setBlockData(Bukkit.createBlockData(material));
-            fallingBlock.setVelocity(new Vector(0, 0.3, 0));
-            fallingBlock.setDropItem(false);
-            fallingBlock.addScoreboardTag(SHIELD_TAG);
-        });
+        FallingBlock fallingBlock = world.spawnFallingBlock(loc, Bukkit.createBlockData(material));
+        fallingBlock.setVelocity(new Vector(0, 0.3, 0));
+        fallingBlock.setDropItem(false);
+        fallingBlock.addScoreboardTag(SHIELD_TAG);
     }
 
     @Override

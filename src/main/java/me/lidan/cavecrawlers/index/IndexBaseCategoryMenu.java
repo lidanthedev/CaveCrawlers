bbbe641f03493441b389e8eca21876b88e5be14b
lidanthedev <ls301205@gmail.com>
2026-01-08 15:02:41 +0200

    Add search functionality to category menus with right-click clear option

--- src/main/java/me/lidan/cavecrawlers/index/IndexBaseCategoryMenu.java
+++ src/main/java/me/lidan/cavecrawlers/index/IndexBaseCategoryMenu.java
@@ -4,13 +4,14 @@ import dev.triumphteam.gui.builder.item.ItemBuilder;
 import dev.triumphteam.gui.guis.Gui;
 import dev.triumphteam.gui.guis.PaginatedGui;
 import me.lidan.cavecrawlers.gui.GuiItems;
+import me.lidan.cavecrawlers.prompt.PromptManager;
 import me.lidan.cavecrawlers.utils.MiniMessageUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 public abstract class IndexBaseCategoryMenu {
     public static ItemBuilder INDEX_GUIDE = ItemBuilder.from(Material.BOOK).name(MiniMessageUtils.miniMessage("<yellow>Index Guide")).lore(MiniMessageUtils.miniMessageList("<yellow>How to Read the drop</yellow>", "<gold>- [Amount] [Drop] ([chance]) [chance modifer] [amount modifer]</gold>", "<yellow>Example:", "<gray>- <gray>1-2 <white>Gold Ingot<gray> (<green>10.00%<gray>) <aqua>✯ <red>✘", "<yellow>You can get 1-2 drops of Gold Ingot", "<yellow>it has 10% and boosted by <aqua>✯ Magic Find", "<yellow>but no amount modifier</yellow>", "<red>✘ means no stat</red>"));
-    public static ItemBuilder SEARCH_ITEM = ItemBuilder.from(Material.COMPASS).name(MiniMessageUtils.miniMessage("<blue>Search Index")).lore(MiniMessageUtils.miniMessageList("<yellow>Click to Search the Index"));
+    public static ItemBuilder SEARCH_ITEM = ItemBuilder.from(Material.COMPASS).name(MiniMessageUtils.miniMessage("<blue>Search Index")).lore(MiniMessageUtils.miniMessageList("<yellow>Click to Search the Index", "<yellow>Right click to clear search"));
     protected final Player player;
     protected final PaginatedGui gui;
     protected final String query;
@@ -31,6 +32,16 @@ public abstract class IndexBaseCategoryMenu {
         // Close item
         gui.setItem(6, 5, GuiItems.CLOSE_ITEM);
         gui.setItem(6, 9, INDEX_GUIDE.asGuiItem());
+        gui.setItem(1, 5, SEARCH_ITEM.asGuiItem(event -> {
+            if (event.isRightClick()) {
+                search("");
+                return;
+            }
+            PromptManager.getInstance().prompt(player, "Search").thenAccept(this::search).exceptionally(throwable -> {
+                search("");
+                return null;
+            });
+        }));
         // Setup GUI items
         setupGui();
         // Next and Previous item
@@ -42,4 +53,6 @@ public abstract class IndexBaseCategoryMenu {
     public void open() {
         gui.open(player);
     }
+
+    public abstract void search(String query);
 }
--- src/main/java/me/lidan/cavecrawlers/index/IndexMobsCategoryMenu.java
+++ src/main/java/me/lidan/cavecrawlers/index/IndexMobsCategoryMenu.java
@@ -3,7 +3,6 @@ package me.lidan.cavecrawlers.index;
 import dev.triumphteam.gui.builder.item.ItemBuilder;
 import me.lidan.cavecrawlers.drops.DropsManager;
 import me.lidan.cavecrawlers.drops.EntityDrops;
-import me.lidan.cavecrawlers.prompt.PromptManager;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -26,13 +25,14 @@ public class IndexMobsCategoryMenu extends IndexBaseCategoryMenu {
             if (!ChatColor.stripColor(mobName.toLowerCase()).contains(query)) continue;
             gui.addItem(ItemBuilder.from(itemGenerator.entityDropsToItemStack(dropsEntry.getValue())).asGuiItem());
         }
-        gui.setItem(1, 5, SEARCH_ITEM.asGuiItem(event -> {
-            PromptManager.getInstance().prompt(player, "Search").thenAccept(result -> {
-                new IndexMobsCategoryMenu(player, result).open();
-            }).exceptionally(throwable -> {
-                new IndexMobsCategoryMenu(player, "").open();
-                return null;
-            });
-        }));
+    }
+
+    @Override
+    public void search(String query) {
+        if (this.query.equals(query)) {
+            // Same query, do nothing
+            return;
+        }
+        new IndexMobsCategoryMenu(player, query).open();
     }
 }

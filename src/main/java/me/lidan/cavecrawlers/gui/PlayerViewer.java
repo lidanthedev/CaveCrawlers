9eb359daeb5a4f908a3b15bd1b49ef49c3b0df0e
LidanTheDev <ls301205@gmail.com>
2026-02-28 00:58:57 +0200

    Refactor PlayerViewer to simplify item display logic in GUI

--- src/main/java/me/lidan/cavecrawlers/gui/PlayerViewer.java
+++ src/main/java/me/lidan/cavecrawlers/gui/PlayerViewer.java
@@ -52,14 +52,10 @@ public class PlayerViewer {
 
         // mainHand
         ItemStack mainHand = player.getInventory().getItemInMainHand();
-        if (mainHand != null) {
-            gui.setItem(1, ItemBuilder.from(mainHand.clone()).asGuiItem());
-        }
-        // mainHand
+        gui.setItem(1, ItemBuilder.from(mainHand.clone()).asGuiItem());
+        // offHand
         ItemStack offhand = player.getInventory().getItemInOffHand();
-        if (offhand != null) {
-            gui.setItem(46, ItemBuilder.from(offhand.clone()).asGuiItem());
-        }
+        gui.setItem(46, ItemBuilder.from(offhand.clone()).asGuiItem());
         // Bank
         gui.setItem(15, ItemBuilder.from(Material.GOLD_INGOT).setName(ChatColor.GRAY + "Money: " + ChatColor.GOLD + StringUtils.getNumberFormat(VaultUtils.getCoins(player))).asGuiItem());
         // Rank

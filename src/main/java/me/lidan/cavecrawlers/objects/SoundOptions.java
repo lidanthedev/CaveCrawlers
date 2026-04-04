fdb0ca98680b3702483c99fd0b722f28de4080d6
LidanTheDev <ls301205@gmail.com>
2026-04-05 01:02:09 +0300

    feat: refactor SoundOptions to use NamespacedKey for sound serialization

--- src/main/java/me/lidan/cavecrawlers/objects/SoundOptions.java
+++ src/main/java/me/lidan/cavecrawlers/objects/SoundOptions.java
@@ -1,6 +1,8 @@
 package me.lidan.cavecrawlers.objects;
 
 import lombok.Data;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.Sound;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.jetbrains.annotations.NotNull;
@@ -19,24 +21,43 @@ public class SoundOptions implements ConfigurationSerializable {
         this.pitch = pitch;
     }
 
+    @SuppressWarnings("unused")
     public SoundOptions(String sound, float volume, float pitch) {
-        this(Sound.valueOf(sound), volume, pitch);
+        this(resolveSound(sound), volume, pitch);
+    }
+
+    public static SoundOptions deserialize(Map<String, Object> map) {
+        Number volume = (Number) map.get("volume");
+        Number pitch = (Number) map.get("pitch");
+        String sound = map.get("sound").toString();
+        return new SoundOptions(resolveSound(sound), volume.floatValue(), pitch.floatValue());
+    }
+
+    private static Sound resolveSound(String sound) {
+        NamespacedKey key = NamespacedKey.fromString(sound);
+        if (key == null) {
+            key = NamespacedKey.minecraft(sound.toLowerCase());
+        }
+
+        Sound resolved = Registry.SOUNDS.get(key);
+        if (resolved == null) {
+            throw new IllegalArgumentException("Unknown sound: " + sound);
+        }
+        return resolved;
     }
 
     @NotNull
     @Override
     public Map<String, Object> serialize() {
+        NamespacedKey soundKey = Registry.SOUNDS.getKey(sound);
+        if (soundKey == null) {
+            throw new IllegalStateException("Cannot serialize unregistered sound: " + sound);
+        }
+
         return Map.of(
-                "sound", sound.name(),
+                "sound", soundKey.toString(),
                 "volume", volume,
                 "pitch", pitch
         );
-    }
-
-    public static SoundOptions deserialize(Map<String, Object> map) {
-        Double volume = (Double) map.get("volume");
-        Double pitch = (Double) map.get("pitch");
-        String sound = map.get("sound").toString();
-        return new SoundOptions(Sound.valueOf(sound), volume.floatValue(), pitch.floatValue());
     }
 }

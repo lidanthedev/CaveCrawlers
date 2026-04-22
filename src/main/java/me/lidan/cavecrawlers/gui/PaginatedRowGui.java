534ed8cfec8fc8cc00f9597943c4573cc1c1f572
LidanTheDev <ls301205@gmail.com>
2026-04-22 15:02:41 +0300

    feat: can change row by override in PaginatedRowGui

--- src/main/java/me/lidan/cavecrawlers/gui/PaginatedRowGui.java
+++ src/main/java/me/lidan/cavecrawlers/gui/PaginatedRowGui.java
@@ -23,17 +23,26 @@ public abstract class PaginatedRowGui {
         List<GuiItem> guiItems = items.subList(currentPage * 7, Math.min(7 * (currentPage + 1), items.size()));
         int size = guiItems.size();
         List<Integer> layoutForItems = getLayoutForItems(size);
-        getLayoutForItems(7).forEach(i -> gui.setItem(3, i, GuiItems.GLASS_ITEM));
+        getLayoutForItems(7).forEach(i -> gui.setItem(getRow(), i, GuiItems.GLASS_ITEM));
         for (int i = 0; i < size; i++) {
-            gui.setItem(3, layoutForItems.get(i), guiItems.get(i));
+            gui.setItem(getRow(), layoutForItems.get(i), guiItems.get(i));
         }
 
 
         if (items.size() > 7) {
-            gui.setItem(3, 1, ItemBuilder.from(Material.ARROW).setName(ChatColor.BLUE + "Previous").asGuiItem(event -> previous()));
-            gui.setItem(3, 9, ItemBuilder.from(Material.ARROW).setName(ChatColor.BLUE + "Next").asGuiItem(event -> next()));
+            gui.setItem(getRow(), 1, ItemBuilder.from(Material.ARROW).setName(ChatColor.BLUE + "Previous").asGuiItem(event -> previous()));
+            gui.setItem(getRow(), 9, ItemBuilder.from(Material.ARROW).setName(ChatColor.BLUE + "Next").asGuiItem(event -> next()));
         }
         gui.update();
+    }
+
+    /**
+     * Override to change the row of the gui
+     *
+     * @return the row of the gui
+     */
+    protected int getRow() {
+        return 3;
     }
 
     public void next() {
@@ -57,7 +66,7 @@ public abstract class PaginatedRowGui {
             case 1 -> List.of(5);
             case 2 -> List.of(4, 6);
             case 3 -> List.of(4, 5, 6);
-            case 4 -> List.of(3, 4, 6, 7);
+            case 4 -> List.of(2, 4, 6, 8);
             case 5 -> List.of(3, 4, 5, 6, 7);
             case 6 -> List.of(2, 3, 4, 6, 7, 8);
             default -> // 7 or more

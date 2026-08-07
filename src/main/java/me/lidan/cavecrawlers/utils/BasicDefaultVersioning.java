55afa94de33e558d14cdc499ec95d7917af7c469
LidanTheDev <ls301205@gmail.com>
2026-08-08 02:18:00 +0300

    feat: add overloaded constructor for BasicDefaultVersioning to support default lastVersion

--- src/main/java/me/lidan/cavecrawlers/utils/BasicDefaultVersioning.java
+++ src/main/java/me/lidan/cavecrawlers/utils/BasicDefaultVersioning.java
@@ -15,6 +15,16 @@ public class BasicDefaultVersioning extends BasicVersioning {
      *
      * @param route the route at which version IDs can be found
      */
+    public BasicDefaultVersioning(@NonNull String route) {
+        this(route, 1);
+    }
+
+    /**
+     * Creates a versioning with the given route.
+     *
+     * @param route the route at which version IDs can be found
+     * @param lastVersion the last version for virtual default
+     */
     public BasicDefaultVersioning(@NonNull String route, int lastVersion) {
         super(route);
         this.route = route;
